e77ce2cae395cfc9ffd299afbcd01e1998ac67fa
Jenna Nguyen <jenna.jtn@gmail.com>
2021-10-25 10:37:29 -0400

    fullstack todo list

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://todoList:leon@cluster0.3jdln.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+    'dbName': 'todoList'
+};
